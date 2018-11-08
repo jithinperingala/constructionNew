07ab7800de42ceed34822a21bd52d8c88c69a29b
jithinRaju <jithin.raju@iinerds.com>
2018-11-08 09:19:01 +0530

    url

--- src/app/configs/app-settings.config.ts
+++ src/app/configs/app-settings.config.ts
@@ -1,5 +1,5 @@
 export const config:any = {
   //  _baseURL:"http://52.15.44.4:3000/",
-  _devbaseURL:'http://localhost:3000/',
-  _prodbaseURL:'https://localhost:3000/'
+  _devbaseURL:'http://localhost:49999/',
+  _prodbaseURL:'https://localhost:30000/'
   }
