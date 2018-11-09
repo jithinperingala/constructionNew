72e85001abedf1621aff203e3a72427d3de609a0
jithinRaju <jithin.raju@iinerds.com>
2018-11-09 10:50:49 +0530

    employee

--- src/app/configs/app-settings.config.ts
+++ src/app/configs/app-settings.config.ts
@@ -1,5 +1,5 @@
 export const config:any = {
   //  _baseURL:"http://52.15.44.4:3000/",
   _devbaseURL:'http://localhost:49999/',
-  _prodbaseURL:'https://localhost:30000/'
+  _prodbaseURL:'https://api.nmpc.co.in:30000/'
   }
