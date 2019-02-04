2103682b067111b55fbf1febe273f82ecaddc809
jithin raju <jithinperingala@gmail.com>
2019-02-04 11:42:11 -0800

    add bak table

--- src/app/shared/components/bank-accounts-details/bank-accounts-details.component.ts
+++ src/app/shared/components/bank-accounts-details/bank-accounts-details.component.ts
@@ -1,21 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms'
 @Component({
   selector: 'app-bank-accounts-details',
   templateUrl: './bank-accounts-details.component.html',
   styleUrls: ['./bank-accounts-details.component.scss']
 })
 export class BankAccountsDetailsComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','ifsc'];
-  dataSource
-  constructor() {
-    this.dataSource = this.ELEMENT_DATA;
-   }
-  ELEMENT_DATA = [
-    {position: 1, name: 'HDFC', weight: '98712367821309', symbol: 'kannur',ifsc:'HDFC1209'},
-    {position: 2, name: 'ICICI', weight: '09723946897349', symbol: 'payannur',ifsc:'ICICI24'},
-  ];
-  ngOnInit() {
+
+
+  bankForm: FormGroup;
+  BANK_DATA: Bank[] = [];
+  constructor(private fb: FormBuilder) {
+
   }
 
+  ngOnInit() {
+    this.bankForm=this.fb.group({
+      name: ["",[]],
+      accNo:["",[]],
+      branch: ["",[]],
+      ifsc: ["",[]],
+    })
+  }
+  addNewBank(data) {
+    this.BANK_DATA.push({
+      id: this.BANK_DATA.length + 1,
+      name: data.name,
+      accNo:  data.accNo,
+      branch:  data.branch,
+      ifsc:  data.ifsc,
+    })
+  }
+  removeBank(id) {
+    this.BANK_DATA.splice(id - 1, 1)
+  }
+
+}
+class Bank {
+  id: number;
+  name: string;
+  accNo: string;
+  branch: string;
+  ifsc: string;
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,12 @@ import { MeterialModule } from './modules/meterial/meterial.module';
 import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
 import { BankAccountsDetailsComponent } from './components/bank-accounts-details/bank-accounts-details.component';
 import { CardHeaderComponent } from './components/card-header/card-header.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    MeterialModule
+    MeterialModule,CommonModule,ReactiveFormsModule,FormsModule
   ],
   exports:[OrderbyPipe,BankAccountsDetailsComponent,CardHeaderComponent],
   declarations: [ OrderbyPipe, BankAccountsDetailsComponent, CardHeaderComponent,]
