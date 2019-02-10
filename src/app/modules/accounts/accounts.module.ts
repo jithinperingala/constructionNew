d25cf63cc650f047deb101fa9ecf3145aac2f5c0
jithin raju <jithinperingala@gmail.com>
2019-02-09 20:32:31 -0800

    fsfd

--- src/app/modules/accounts/accounts.module.ts
+++ src/app/modules/accounts/accounts.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { AccountsRoutingModule } from "./accounts-routing.module";
-import { MeterialPurchaseComponent } from "./meterial-purchase/meterial-purchase.component";
-import { ItemDetailsComponent } from "./item-details/item-details.component";
-import { UnitDetailsComponent } from "./unit-details/unit-details.component";
 import { PaymentComponent } from "./payment/payment.component";
 import { PaymentViewComponent } from "./payment/payment-view/payment-view.component";
 import { SharedModule } from "src/app/shared/shared.module";
@@ -24,9 +21,6 @@ import { LaborPaymentComponent } from "./payment/labor-payment/labor-payment.com
     ReactiveFormsModule
   ],
   declarations: [
-    MeterialPurchaseComponent,
-    ItemDetailsComponent,
-    UnitDetailsComponent,
     PaymentComponent,
     PaymentViewComponent,
     PettycashbookComponent,
@@ -34,4 +28,4 @@ import { LaborPaymentComponent } from "./payment/labor-payment/labor-payment.com
     LaborPaymentComponent
   ]
 })
-export class AccountsModule {}
+export class AccountsModule { }
